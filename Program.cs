baf21f6700001b72748cd5ca50ce4b1602d7c34d
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-09-21 09:06:30 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//*****************************************************************************
+//** 386. Lexicographical Numbers   leetcode                                 **
+//*****************************************************************************
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* lexicalOrder(int n, int* returnSize) {
+    int* retNum = (int*)malloc(n * sizeof(int));
+    int v = 1;
+
+    for (int i = 0; i < n; i++) {
+        retNum[i] = v;
+        if (v * 10 <= n) {
+            v *= 10;
+        } else {
+            while (v % 10 == 9 || v == n) {
+                v /= 10;
+            }
+            v++;
+        }
+    }
+
+    *returnSize = n;
+    return retNum;
+}
